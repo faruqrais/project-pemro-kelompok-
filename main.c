92717d897dcd46292fb996fabbc2d46585c62a70
faruqrais <ffrqrz@gmail.com>
2024-11-23 13:43:35 +0700

    Update main.c
    menambahkan fitur login

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>                                                                            // Untuk strcmp
 
-int main(){
+int main() {
+    char username[100];
+    char password[100];
+
+    printf("Masukkan username Anda: ");
+    scanf("%s", username); 
+
+    printf("Masukkan password Anda: ");
+    scanf("%s", password);                                                                    // Membaca password
+
+    
+    if ((strcmp(username, "kelompok") == 0) && (strcmp(password, "admin123") == 0)) {
+        printf("Selamat Anda berhasil login\n");
+    } else {
+        printf("Maaf, username atau password Anda salah\n");
+    }
 
     return 0;
 }
