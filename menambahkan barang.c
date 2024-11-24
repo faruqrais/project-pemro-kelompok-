2623e935be5ff02f15bdb688421d5219fe7f9ffa
faruqrais <ffrqrz@gmail.com>
2024-11-24 13:20:49 +0700

    Update menambahkan barang.c

--- menambahkan barang.c
+++ menambahkan barang.c
@@ -13,26 +13,6 @@ typedef struct {
 Item store[MAX_ITEMS];
 int itemCount = 0;
 
-// Fungsi login
-int login() {
-    char username[100];
-    char password[100];
-
-    printf("Masukkan username Anda: ");
-    scanf("%s", username);
-
-    printf("Masukkan password Anda: ");
-    scanf("%s", password);
-
-    if ((strcmp(username, "kelompok") == 0) && (strcmp(password, "empat") == 0)) {
-        printf("Selamat, Anda berhasil login!\n\n");
-        return 1;
-    } else {
-        printf("Maaf, username atau password Anda salah.\n");
-        return 0;
-    }
-}
-
 // Menambahkan barang baru
 void tambahbarang() {
     if (itemCount >= MAX_ITEMS) {
@@ -91,8 +71,6 @@ void menu() {
 }
 
 int main() {
-    if (login()) {
-        menu();
-    }
+    menu();
     return 0;
 }
